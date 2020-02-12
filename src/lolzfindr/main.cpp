32bb43833f3179bc0d1ee24c3ab0935d745332dd
Ian Parker <ian@geekprojects.com>
2020-02-12 17:43:40 +0000

    lolzfindr: Basic error checking and config via getopts

--- src/lolzfindr/main.cpp
+++ src/lolzfindr/main.cpp
@@ -4,16 +4,52 @@
 #include <yaml-cpp/yaml.h>
 
 #include <stdio.h>
+#include <getopt.h>
 #include <inttypes.h>
 
 using namespace std;
 using namespace Geek;
 using namespace Geek::Core;
 
+static const struct option g_options[] =
+{
+    { "config",  required_argument, NULL, 'c' },
+    { NULL,      0,                 NULL, 0 }
+};
+
 int main(int argc, char** argv)
 {
     string configPath;
     configPath = string(getenv("HOME")) + "/.lolz.yml";
+
+    while (true)
+    {
+        int c = getopt_long(
+            argc,
+            argv,
+            "c:",
+            g_options,
+            NULL);
+        if (c == -1)
+        {
+            break;
+        }
+        switch (c)
+        {
+            case 'c':
+                configPath = string(optarg);
+                break;
+
+        }
+    }
+
+    int remaining_argc = argc - optind;
+    if (remaining_argc <= 0)
+    {
+        printf("%s <search terms>\n", argv[0]);
+        exit(1);
+    }
+
     YAML::Node m_config = YAML::LoadFile(configPath);
 
     string dbpath = m_config["database"].as<std::string>();
@@ -22,6 +58,11 @@ int main(int argc, char** argv)
 
     bool res;
     res = db->open();
+    if (!res)
+    {
+        printf("Oh noes! Where iz the database?\n");
+        return 1;
+    }
 
     string keyword = argv[1];
 
@@ -39,5 +80,7 @@ int main(int argc, char** argv)
     delete ps;
 
     delete db;
+
+    return 0;
 }
 
